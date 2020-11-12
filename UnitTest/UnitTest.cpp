88a3a9b5327cf899949fbaede9a63621aeb5ab7f
SergiyStepanchuk <serhii.stepanchuk.itis.2020@lpnu.ua>
2020-11-12 20:52:45 +0200

    init

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Program/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+
+		TEST_METHOD(met_do_stuff)
+		{
+			int mass[5] = { 1, 2, 2, 2, 1 };
+			Assert::AreEqual(do_stuff(mass, 5, 0, 0), 2.);
+		}
+	};
+}
